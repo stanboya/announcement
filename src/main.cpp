400460d0d3da601833a8104b058808a124d3f176
John Agapeyev <jagapeyev@gmail.com>
2018-07-05 12:48:54 -0700

    Added getopt framework for args parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstdint>
+#include <variant>
+#include <unistd.h>
+#include <getopt.h>
+
+static struct option long_options[] = {
+    {"verbose",     no_argument,       0, 'v'},
+    {"output",      required_argument, 0, 'o'},
+    {0,             0,                 0,  0}
+};
+
+#define print_help() \
+    do { \
+        printf("usage options:\n"\
+                "\t [v]erbose               - Output in verbose mode\n"\
+                "\t [o]utput                - File to output revised beliefse to\n"\
+                "\t [h]elp                  - this message\n"\
+                );\
+    } while(0)
 
 int main(int argc, char **argv) {
-    std::cout << "Hello world!\n";
+    const char *output_file = nullptr;
+    bool verbose = false;
+    for (;;) {
+        int c;
+        int option_index = 0;
+        if ((c = getopt_long(argc, argv, "hvo:", long_options, &option_index)) == -1) {
+            break;
+        }
+        switch (c) {
+            case 'v':
+                verbose = true;
+                break;
+            case 'o':
+                output_file = optarg;
+                break;
+            case 'h':
+                [[fallthrough]];
+            case '?':
+                [[fallthrough]];
+            default:
+                print_help();
+                return EXIT_SUCCESS;
+        }
+    }
+
     return EXIT_SUCCESS;
 }
