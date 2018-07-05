425911eb4244fc06cecddf164f62a11461fbdcb9
John Agapeyev <jagapeyev@gmail.com>
2018-07-05 15:49:33 -0700

    Added announce file

---
+++ src/announce.cpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include "utils.h"
+#include "tools.h"
+#include "announce.h"
+
+void find_announcement(const std::vector<agent>& agents) noexcept {
+
+}
+
+
---
+++ src/announce.h
@@ -0,0 +1,17 @@
+#ifndef ANNOUNCE_H
+#define ANNOUNCE_H
+
+#include <vector>
+#include <cstdint>
+
+struct agent {
+    //Both of these vectors are in DNF
+    std::vector<std::vector<int32_t>> beliefs;
+    std::vector<std::vector<int32_t>> goal;
+};
+
+void find_announcement(const std::vector<agent>& agents) noexcept;
+
+
+
+#endif
