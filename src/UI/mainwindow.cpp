e5a216086be1925119a837d4c9f25c205b7bbcbc
John Agapeyev <jagapeyev@gmail.com>
2018-08-01 10:45:52 -0700

    Added generation of table rows

--- src/UI/mainwindow.cpp
+++ src/UI/mainwindow.cpp
@@ -1,4 +1,8 @@
+#include <QInputDialog>
+#include <QDir>
+#include <QTableWidgetItem>
 #include <iostream>
+
 #include "add_entry_dialog.h"
 #include "mainwindow.h"
 #include "ui_add_entry_dialog.h"
@@ -13,12 +17,38 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::on_add_entry_clicked() {
-    add_entry_dialog d;
-    d.exec();
+    bool ok;
+    QString beliefs = QInputDialog::getText(this, tr("Initial Agent Beliefs"), tr("Initial Beliefs:"),
+        QLineEdit::Normal, QDir::home().dirName(), &ok);
+
+    if (ok && !beliefs.isEmpty()) {
+        std::cout << beliefs.toStdString() << "\n";
+    }
+    QString goals = QInputDialog::getText(this, tr("Desired Agent Goal"), tr("Desired Goal:"),
+        QLineEdit::Normal, QDir::home().dirName(), &ok);
+
+    if (ok && !goals.isEmpty()) {
+        std::cout << goals.toStdString() << "\n";
+    }
+
+    std::cout << "Done info gathering\n";
+
+    ui->data_table->insertRow(ui->data_table->rowCount());
+    QTableWidgetItem *item = new QTableWidgetItem();
+    item->setCheckState(Qt::Unchecked);
+    ui->data_table->setItem(ui->data_table->rowCount() - 1, 0, item);
+    ui->data_table->setItem(ui->data_table->rowCount() - 1, 1, new QTableWidgetItem(QString::number(7)));
+    ui->data_table->setItem(ui->data_table->rowCount() - 1, 2, new QTableWidgetItem(beliefs));
+    ui->data_table->setItem(ui->data_table->rowCount() - 1, 3, new QTableWidgetItem(goals));
 }
 
 void MainWindow::on_remove_entry_clicked() {
-    std::cout << "\n";
+    QItemSelectionModel *selectionModel = ui->data_table->selectionModel();
+    QModelIndexList selectedRows = selectionModel->selectedRows();
+    if (selectedRows.size() > 0) {
+        // There is at lease one selected row.
+        std::cout << "Have something to remove\n";
+    }
 }
 
 void MainWindow::on_calculate_annoouncement_clicked() {
